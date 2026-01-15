9d3700f10612d4c06f881d24d1128ffdb4d44b96
DEEPAK KUMAR <deepakkumar@DEEPAKs-MacBook-Air.local>
2026-01-15 16:40:35 +0530

    class components with lifecycle methods

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,44 @@
 
 import User from "./User";
 import UserClass from "./UserClass";
+import React from "react";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About us</h1>
-      <h2>This is Namaste React course.</h2>
-      {/* <User name={"Deepak Kumar (function)"}/> */}
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent - constructor");
+  }
 
-      <UserClass name={"Deepak Kumar (class)"} location={"Bangalore (class)"}/>
-    </div>
-  );
-};
+componentDidMount() {
+    console.log("Parent - componentDidMount");
+  }
+
+  render() {
+    console.log("Parent - render");
+    return (
+      <div>
+        <h1>About us</h1>
+        <h2>This is Namaste React course.</h2>
+        {/* <User name={"Deepak Kumar (function)"}/> */}
+
+        <UserClass name={"Deepak Kumar (class)"} location={"Bangalore (class)"}/>
+      </div>
+    );
+  }
+}
+
+
+
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About us</h1>
+//       <h2>This is Namaste React course.</h2>
+//       {/* <User name={"Deepak Kumar (function)"}/> */}
+
+//       <UserClass name={"Deepak Kumar (class)"} location={"Bangalore (class)"}/>
+//     </div>
+//   );
+// };
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,13 +6,26 @@ class UserClass extends React.Component {
     console.log(props);
     this.state = {
       count: 0,
-      count2: 0,
+      userInfo:{}
     };
+    console.log("Child - constructor");
+  }
+
+async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/deepakssit022");
+    const json = await data.json();
+    console.log(json);
+    this.setState({
+        userInfo: json
+    })
+    console.log("Child - componentDidMount");
   }
 
   render() {
     const { name, location } = this.props;
-    const { count, count2 } = this.state;
+    const { count, userInfo } = this.state;
+
+    console.log("Child - render");
     return (
       <div className="user-card">
         <h1>Count: {count}</h1>
@@ -21,8 +34,8 @@ class UserClass extends React.Component {
                 count: count + 1
             })
         }}>Count Increase</button>
-        <h2>Name: {name}</h2>
-        <h3>Location: {location}</h3>
+        <h2>Name: {userInfo.name}</h2>
+        <h3>Location: {userInfo.location}</h3>
         <h3>Contact: 9876543210</h3>
       </div>
     );
